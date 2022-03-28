3ce551432d7a492e668b07b2aa229b0453ddfc40
Hamed Tavakoli <preloopadm@gmail.com>
2022-03-28 19:10:32 +0000

    curl ssl fix
    the headers for curl request are not necessarily needed; i just did not bother removing them

--- data.php
+++ data.php
@@ -4,8 +4,6 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, 'https://formulae.brew.sh/api/formula.json');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 
 curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
 
